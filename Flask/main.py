8cf3470a74cacf97d6d2f1e0bec0ff147d578b8f
Tuan Minh Nguyen <tuanmminh.101112@gmail.com>
2024-04-17 11:13:29 +0700

    first

---
+++ Flask/main.py
@@ -0,0 +1,45 @@
+from pakages import app
+from flask import render_template, request
+import utils
+
+@app.route("/")
+def home():
+    cats = utils.load_categories()
+    return render_template("index.html",
+                           categories = cats)
+
+
+@app.route("/conferences")
+def conferences_list():
+    cate_id = int(request.args.get("category_id"))
+    conference = utils.load_conferences(cats = cate_id)
+
+    if cate_id == 1:
+        return deadline_list(conference)
+    elif cate_id == 2:
+        return running_list(conference)
+    elif cate_id == 3:
+        return future_list(conference)
+    else:
+        return planned_list(conference)
+
+
+def deadline_list(conferences):
+    return render_template("deadline.html",
+                           conf = conferences)
+
+def running_list(conferences):
+    return render_template("running.html",
+                           conf = conferences)
+
+def future_list(conferences):
+    return render_template("future.html",
+                           conf = conferences)
+
+def planned_list(conferences):
+    return render_template("planned.html",
+                           conf = conferences)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
